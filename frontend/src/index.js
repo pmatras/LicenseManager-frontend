6bdda99a38052ba6a10312c52702d6d65e5df837
Piotr Matras <piotrmatras21@gmail.com>
2020-10-31 12:52:10 +0100

    project initialized with create-react-app and setted up

---
+++ frontend/src/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+ReactDOM.render(
+  <React.StrictMode>
+   <div></div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+reportWebVitals();
