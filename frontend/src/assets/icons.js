466af4a4d23eafc9717cfdaf49921210bb759a1d
Piotr Matras <piotrmatras21@gmail.com>
2020-11-11 15:51:55 +0100

    added menu icons

---
+++ frontend/src/assets/icons.js
@@ -0,0 +1,5 @@
+import licensesIcon from './licensesIcon.svg';
+import securityIcon from './securityIcon.svg';
+import settingsIcon from './settingsIcon.svg';
+
+export { licensesIcon, securityIcon, settingsIcon };
