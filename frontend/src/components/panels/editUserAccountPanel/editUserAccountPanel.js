805f99b83805433d381f0bcd5151617bd8a53c1a
Piotr Matras <piotrmatras21@gmail.com>
2020-12-28 14:39:36 +0100

    fixed layout on mobile devices

--- frontend/src/components/panels/editUserAccountPanel/editUserAccountPanel.js
+++ frontend/src/components/panels/editUserAccountPanel/editUserAccountPanel.js
@@ -100,7 +100,7 @@ const EditUserAccountPanel = ({
   };
 
   return (
-    <EuiPanel style={{ width: '40%', marginLeft: '30%', marginRight: '30%' }}>
+    <EuiPanel>
       <EuiFlexGroup
         direction="column"
         justifyContent="center"
@@ -109,60 +109,62 @@ const EditUserAccountPanel = ({
         <EuiFlexItem>
           <EuiAvatar name={`${firstName} ${lastName}`} size="xl" />
         </EuiFlexItem>
+        <EuiSpacer size="xl" />
+        <EuiFlexItem>
+          <EuiForm component="div" isInvalid={!!error} error={error}>
+            <EuiFormRow label="Confirm your password">
+              <EuiFieldPassword
+                readOnly={isFormReadOnly}
+                name="currentPassword"
+                placeholder="Confirm Password"
+                value={userDetails.currentPassword}
+                type="dual"
+                onChange={formHandler}
+              />
+            </EuiFormRow>
+            <EuiSpacer size="xl" />
+            <EuiFormRow label="Change username">
+              <EuiFieldText
+                readOnly={isFormReadOnly}
+                name="username"
+                value={userDetails.username}
+                onChange={formHandler}
+              />
+            </EuiFormRow>
+            <EuiFormRow label="Change Password">
+              <EuiFieldPassword
+                readOnly={isFormReadOnly}
+                name="password"
+                placeholder="Password"
+                value={userDetails.password}
+                type="dual"
+                onChange={formHandler}
+              />
+            </EuiFormRow>
+            <EuiFormRow label="Confirm Password">
+              <EuiFieldPassword
+                readOnly={isFormReadOnly}
+                name="rePassword"
+                placeholder="Re-enter password"
+                value={userDetails.rePassword}
+                type="dual"
+                onChange={formHandler}
+              />
+            </EuiFormRow>
+            <EuiSpacer size="xl" />
+            <EuiFlexGroup>
+              <EuiFlexItem>
+                <EuiButton onClick={toggleFormReadOnly}>Edit Account</EuiButton>
+              </EuiFlexItem>
+              <EuiFlexItem>
+                <EuiButton fill onClick={editUserAccount}>
+                  Save
+                </EuiButton>
+              </EuiFlexItem>
+            </EuiFlexGroup>
+          </EuiForm>
+        </EuiFlexItem>
       </EuiFlexGroup>
-      <EuiSpacer size="xl" />
-      <EuiForm component="div" isInvalid={!!error} error={error}>
-        <EuiFormRow label="Confirm your password">
-          <EuiFieldPassword
-            readOnly={isFormReadOnly}
-            name="currentPassword"
-            placeholder="Confirm Password"
-            value={userDetails.currentPassword}
-            type="dual"
-            onChange={formHandler}
-          />
-        </EuiFormRow>
-        <EuiSpacer size="xl" />
-        <EuiFormRow label="Change username">
-          <EuiFieldText
-            readOnly={isFormReadOnly}
-            name="username"
-            value={userDetails.username}
-            onChange={formHandler}
-          />
-        </EuiFormRow>
-        <EuiFormRow label="Change Password">
-          <EuiFieldPassword
-            readOnly={isFormReadOnly}
-            name="password"
-            placeholder="Password"
-            value={userDetails.password}
-            type="dual"
-            onChange={formHandler}
-          />
-        </EuiFormRow>
-        <EuiFormRow label="Confirm Password">
-          <EuiFieldPassword
-            readOnly={isFormReadOnly}
-            name="rePassword"
-            placeholder="Re-enter password"
-            value={userDetails.rePassword}
-            type="dual"
-            onChange={formHandler}
-          />
-        </EuiFormRow>
-        <EuiSpacer size="xl" />
-        <EuiFlexGroup>
-          <EuiFlexItem>
-            <EuiButton onClick={toggleFormReadOnly}>Edit Account</EuiButton>
-          </EuiFlexItem>
-          <EuiFlexItem>
-            <EuiButton fill onClick={editUserAccount}>
-              Save
-            </EuiButton>
-          </EuiFlexItem>
-        </EuiFlexGroup>
-      </EuiForm>
     </EuiPanel>
   );
 };
